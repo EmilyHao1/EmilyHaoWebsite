a6185bc3a3b4a58be5dc0cd688fff0ffea0f372e
Emily Hao <Emilyhaomac@Emilys-MacBook-Pro-4.local>
2017-11-05 02:11:20 -0500

    change of layout

--- server.js
+++ server.js
@@ -13,24 +13,27 @@ var server = http.createServer (function (req, res) {
       case '/index.html':
         sendFile(res, 'index.html')
          break
-      case '/self_introduction.png':
-          sendFile(res, 'self_introduction.png')
-          break
-      case '/activities.jpeg':
-          sendFile(res, 'activities.jpeg')
-          break
-      case '/computer-science-jobs.png':
-          sendFile(res, 'computer-science-jobs.png')
-          break
-      case '/Work-Experience.jpg':
-          sendFile(res, 'Work-Experience.jpg')
-          break
-      case '/major.jpeg':
-          sendFile(res, 'major.jpeg')
-          break
-      case '/project.jpeg':
-          sendFile(res, 'project.jpeg')
-          break
+      case 'css/style.css':
+        sendFile(res, '../css/style.css')
+         break
+      // case '/self_introduction.png':
+      //     sendFile(res, 'self_introduction.png')
+      //     break
+      // case '/activities.jpeg':
+      //     sendFile(res, 'activities.jpeg')
+      //     break
+      // case '/computer-science-jobs.png':
+      //     sendFile(res, 'computer-science-jobs.png')
+      //     break
+      // case '/Work-Experience.jpg':
+      //     sendFile(res, 'Work-Experience.jpg')
+      //     break
+      // case '/major.jpeg':
+      //     sendFile(res, 'major.jpeg')
+      //     break
+      // case '/project.jpeg':
+      //     sendFile(res, 'project.jpeg')
+      //     break
       default:
       res.end('404 not found')
   }
